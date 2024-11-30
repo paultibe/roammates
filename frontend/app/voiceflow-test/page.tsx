7d2191a3ef799b274ab01a4b08771c090bef8d21
Paul Tiberghien <paultiberghien1@gmail.com>
2024-11-30 18:16:36 -0500

    add AI agent

--- frontend/app/voiceflow-test/page.tsx
+++ frontend/app/voiceflow-test/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 // imports
 import { useEffect } from "react";
+import styles from "./voiceflow.module.css";
 
 const Page = () => {
   useEffect(() => {
@@ -11,12 +12,19 @@ const Page = () => {
       v.onload = function () {
         // @ts-expect-error - Voiceflow types not available
         window.voiceflow.chat.load({
-          verify: { projectID: "674b1ac845ea06386b0b60b8" },
+          verify: { projectID: "674b8e45bb2730de151d6ae0" },
           url: "https://general-runtime.voiceflow.com",
           versionID: "production",
+          // assistant: {
+          //   title: "string",
+          //   description: "string",
+          //   image: "string",
+          //   // color: "string",
+          //   stylesheet: "styles", 
+          // },
           render: {
             mode: "embedded",
-            target: document.getElementById("voiceflow-test"),
+            target: document.getElementById("voiceflow"),
           },
           autostart: false,
         });
@@ -41,10 +49,9 @@ const Page = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
       <div
-        id="voiceflow-test"
+        id="voiceflow"
         className="w-4/5 h-[600px] bg-white rounded-lg border-2 border-gray-300 shadow-lg p-6"
       >
-        Voiceflow div
       </div>
     </div>
   );
