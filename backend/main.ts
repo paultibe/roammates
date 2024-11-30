78545cb826f82b45d795de1c502add36adf6af7f
Paul Tiberghien <paultiberghien1@gmail.com>
2024-11-30 06:25:09 -0500

    setup backend

---
+++ backend/main.ts
@@ -0,0 +1,11 @@
+// @deno-types="npm:@types/express@4.17.15"
+import express from "npm:express@4.18.2";
+
+const app = express();
+
+// ENDPOINTS 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Dinosaur API!");
+});
+
+app.listen(8000);
